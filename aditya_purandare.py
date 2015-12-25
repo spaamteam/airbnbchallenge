86d6fd701fd48d2671748fde899f801d1115a064
Aditya Purandare <aditya.p1993@hotmail.com>
2015-12-25 01:11:04 -0600

    Add code for initial extraction to data frames

--- aditya_purandare.py
+++ aditya_purandare.py
@@ -2,3 +2,21 @@ import numpy as np
 import pandas as pd
 
 print("Howdy, Aggies!")
+
+# files = ['countries.csv', 'sessions.csv', 'age_gender_bkts.csv', 'train_users.csv', 'test_users.csv']
+# Lighter version of file import
+files = ['countries.csv', 'age_gender_bkts.csv', 'train_users.csv', 'test_users.csv']
+data_frame = {}
+
+for file in files:
+    data_frame[file.replace('.csv', '')] = pd.read_csv('data/' + file)
+
+train = data_frame['train_users']
+test = data_frame['test_users']
+train = train.fillna(-1)
+test = test.fillna(-1)
+
+labels = train['country_destination'].values
+print(labels)
+
+print("Program finished")
