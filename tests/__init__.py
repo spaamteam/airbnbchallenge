c9a0aa6af76da43f2921cb83fb025c3b716a56b3
Aditya Purandare <aditya.p1993@hotmail.com>
2016-01-06 15:30:25 -0600

    Build fails

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,13 +7,3 @@ class InitializationTests(unittest.TestCase):
         Check the test suite runs by affirming 2+2=4
         """
         self.assertEqual(2+2, 4)
-
-
-    def test_import(self):
-        """
-        Ensure the test suite can import our module
-        """
-        try:
-            import "../airbnb"
-        except ImportError:
-            self.fail("Was not able to import the airbnb")
